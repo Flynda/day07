a303b719549007c85946903075f8a792e39d4a9a
Linda See <seewenli@gmail.com>
2020-11-04 14:00:36 +0800

    2 versions

--- main_v2.js
+++ main_v2.js
@@ -2,8 +2,29 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const mysql = require('mysql2/promise')
 
-const SQL_LIST_BY_NAME = 'select tvid, name from tv_shows order by name desc limit ? offset ?'
+// configurables
+const LIMIT = 30
+
+const SQL_LIST_BY_NAME = 'select tvid, name from tv_shows order by name desc limit ?'
 const SQL_GET_TV_SHOWS_BY_TVID = 'select * from tv_shows where tvid = ?'
+
+const mkQuery = (sqlStmt, pool) => {
+    const f = async (params) => {
+        // get a connection from the pool
+        const conn = await pool.getConnection()
+
+        try {
+            const results = await pool.query(sqlStmt, params)
+            return results[0]
+        } catch(e) {
+            return Promise.reject(e)
+        } finally {
+            conn.release()
+        }
+    }
+    return f
+}
+
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000
 
 const pool = mysql.createPool({
@@ -15,6 +36,12 @@ const pool = mysql.createPool({
     connectionLimit: 4,
     timezone: '+08:00'
 })
+
+// create queries 
+const getTVList = mkQuery(SQL_LIST_BY_NAME, pool)
+const getTVShowById = mkQuery(SQL_GET_TV_SHOWS_BY_TVID, pool)
+
+
 
 const startApp = async (app, pool) => {
     try {
@@ -39,62 +66,57 @@ app.set('view engine', 'hbs')
 
 // configure the application
 app.get('/', async (req, resp) => {
-    const conn = await pool.getConnection()
 
     try {
-        const [results, _] = await conn.query(SQL_LIST_BY_NAME, [20, 0])
+        const result = await getTVList([LIMIT])
+        console.info(`main: `, result)
 
         resp.status(200)
         resp.type('text/html')
-        resp.render('landing', {tv_show: results})
+        resp.render('landing', {tv_show: result})
     } catch(e) {
         resp.status(500)
         resp.type('text/html')
         resp.send(JSON.stringify(e))
-    } finally {
-        conn.release()
     }
     
 })
 
 app.get('/tv_show/:tvid', async (req, resp) => {
     const tvid = req.params['tvid']
-    const conn = await pool.getConnection()
 
     try {
-        const [results, _] = await conn.query(SQL_GET_TV_SHOWS_BY_TVID, [tvid])
-
-        if (results.length <= 0) {
+        const result = await getTVShowById([tvid])
+        console.info(`result: `, result)
+        if (result.length <= 0) {
             resp.status(404)
             resp.type('text/html')
             resp.send(`Not found: ${tvid}`)
         }
-        console.info('url:', results[0].official_site)
+
         resp.status(200)
         resp.format({
             'text/html': () => {
                 resp.type('text/html')
                 resp.render('detail', {
-                    tv_shows: results[0],
+                    tv_shows: result[0],
                     hasOfficialSite: !!results[0].official_site
                 })
 
             },
             'application/json': () => {
                 resp.type('application/json')
-                resp.json(results[0])
+                resp.json(result[0])
             },
             'default': () => {
                 resp.type('text/plain')
-                resp.send(JSON.stringify(results[0]))
+                resp.send(JSON.stringify(result[0]))
             }
         })
     } catch(e) {
         resp.status(500)
         resp.type('text/html')
         resp.send(JSON.stringify(e))
-    } finally {
-        conn.release()
     }
 })
 
